253910921a5d31eab9d722ad58b30656031e8ffe
elena <elena.parteni@gmail.com>
2024-07-30 13:01:59 +0300

    Adaugare securitate.

---
+++ src/main/java/com/hospital/hospital_system/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.hospital.hospital_system.security;
+
+import com.hospital.hospital_system.models.User;
+import com.hospital.hospital_system.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUname(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getUtip());
+        return new org.springframework.security.core.userdetails.User(
+                user.getUname(),
+                user.getParola(),
+                Collections.singletonList(authority)
+        );
+    }
+
+
+}
--- src/main/java/com/hospital/hospital_system/security/SecurityConfig.java
+++ src/main/java/com/hospital/hospital_system/security/SecurityConfig.java
@@ -1,55 +1,56 @@
 package com.hospital.hospital_system.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.http.HttpMethod;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, AuthenticationConfiguration authenticationConfiguration) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers(HttpMethod.GET).authenticated()
+                        .requestMatchers("/admin/**").hasRole("Admin")
+                        .requestMatchers("/doctor/**").hasRole("Doctor")
+                        .requestMatchers("/asistent/**").hasRole("Asistent")
+                        .requestMatchers("/pacient/**").hasRole("Pacient")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());;
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(customUserDetailsService);
 
         return http.build();
     }
 
-
     @Bean
-    public UserDetailsService users() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("password"))
-                .roles("Admin")
-                .build();
-        return new InMemoryUserDetailsManager(admin);
-    }
-
-    @Bean
-
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+
+    }
 }
-
-
