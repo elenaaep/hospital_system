086fdb5f5140bb93c28a929aa7fe008ab24605be
elena <elena.parteni@gmail.com>
2024-08-07 14:51:25 +0300

    Adaugare securitate.

--- src/main/java/com/hospital/hospital_system/controller/LoginController.java
+++ src/main/java/com/hospital/hospital_system/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.hospital.hospital_system.controller;
 import com.hospital.hospital_system.models.User;
 import com.hospital.hospital_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ public class LoginController {
 
     @Autowired
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String showLoginForm(Model model) {
@@ -27,13 +29,17 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@RequestParam String uname, @RequestParam String password, Model model) {
         Optional<User> optionalUser = userRepository.findByUname(uname);
-        if (optionalUser.isPresent() && optionalUser.get().getParola().equals(password)) {
+        if (optionalUser.isPresent() && passwordEncoder.matches(password, optionalUser.get().getParola())) {
             model.addAttribute("message", "Login successful!");
-            return "redirect:/doctor"; //  pagina dupa autentificare
+            System.out.println("User found: " + optionalUser.get());
+            System.out.println("Password check: " + passwordEncoder.matches(password, optionalUser.get().getParola()));
+
+            return "redirect:/doctor"; // Redirect către pagina dupa autentificare
         } else {
             model.addAttribute("error", true);
             model.addAttribute("message", "Invalid username or password");
-            return "login"; // ramai pe pagina de login cu mesaj de eroare
+            return "login"; // Ramai pe pagina de login cu mesaj de eroare
         }
     }
+
 }
